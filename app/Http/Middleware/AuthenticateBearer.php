4be853951ff8f1ba87ebd0828e60de58b63c649c
Bruno Freire <brunoqm99@gmail.com>
2026-01-16 12:57:43 -0300

    Enhance AuthenticateBearer middleware by replacing debug output with logging for bearer token and environment variable, improving traceability and readability.

--- app/Http/Middleware/AuthenticateBearer.php
+++ app/Http/Middleware/AuthenticateBearer.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
 
 class AuthenticateBearer
 {
@@ -39,8 +40,8 @@ class AuthenticateBearer
         $token = $request->bearerToken();
 
 
-        var_dump($authBearer);
-        var_dump($token);
+        Log::info('authBearer: ' . $authBearer);
+        Log::info('token: ' . $token);
 
 
         if (!$token || $token !== $authBearer) {
