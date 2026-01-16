c7cc556b4a4799d37e5ebd122630581b4f2a5303
Bruno Freire <brunoqm99@gmail.com>
2026-01-16 06:09:39 -0300

    Refactor middleware configuration to ensure proper integration of ServerTimingMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->prepend(\BeyondCode\ServerTiming\Middleware\ServerTimingMiddleware::class);
-        
+
         $middleware->alias([
             'auth.bearer' => \App\Http\Middleware\AuthenticateBearer::class,
         ]);
